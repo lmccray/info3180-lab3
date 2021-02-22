9d6be1b32fb2aa965ba26d816a7d91fbcfbba80e
lmccray <lmccray@github.com>
2021-02-21 23:37:38 -0500

    Updated Files

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,13 @@
-from os import name
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import InputRequired, Length, Email
+from werkzeug.utils import secure_filename
+
 
 class ContactForm(FlaskForm):
-    name = StringField('name',validators=[DataRequired()])
-    email = StringField('email',validators=[DataRequired()])
-    subject = StringField('subject',validators=[DataRequired()])
-    message = StringField('message',validators=[DataRequired()])
-    csrf=True
+    name = StringField('name', validators=[InputRequired(), Length(max=70)])
+    email = EmailField('email', validators=[Email(), InputRequired(), Length(max=80)])
+    subject = StringField('subject', validators=[InputRequired(), Length(max=70)])
+    message = TextAreaField('message', validators=[InputRequired(), Length(max=80)])
 
--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ from app import app
 from flask import render_template, request, redirect, url_for, flash
 from app import mail
 from flask_mail import Message
+from app.forms import ContactForm
 
 
 ###
@@ -26,13 +27,17 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Leonard McCray")
 
-from forms import ContactForm
-@app.route('/contact', methods=['GET', 'POST'])
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
+    """Render the website's contact page."""
     form = ContactForm()
     if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('contact.html',form=form)
+        msg = Message(request.form['subject'], sender=(request.form['name'], request.form['email']), recipients=['d4f526d9b3-e315b4@inbox.mailtrap.io'])
+        msg.body = request.form['message']
+        mail.send(msg)
+        flash('Message was sent successfully.')
+        return redirect(url_for('home'))        
+    return render_template('contact.html', form = form)
 
 ###
 # The functions below should be applicable to all Flask apps.
